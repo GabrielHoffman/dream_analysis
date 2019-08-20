c65a7c7fc6593bbfdb6d2c5bcd3f338f6f880ff0
GabrielHoffman <gabrielhoffman@typhon.fios-router.home>
2019-08-20 08:51:13 -0400

    update

--- sims/generate_simulations.R
+++ sims/generate_simulations.R
@@ -173,7 +173,7 @@ assignInNamespace('sgseq', function(x,...){1}, "polyester")
 # meanmodel=FALSE,
 
 FC_scale = t(apply(FC, 1, function(x){
-	x = x
+	x = x / 3
 	x - min(x) + 1
 	})) 
 
@@ -197,7 +197,8 @@ FC_scale = t(apply(FC, 1, function(x){
 
 
 polyester::simulate_experiment(opt$fasta, transcripts=fastaTranscripts, 
-	reads_per_transcript = 2^runif(length(fastaTranscripts), 2, 14),
+	# reads_per_transcript = 2^runif(length(fastaTranscripts), 2, 14),
+	 meanmodel=TRUE,
     num_reps = as.matrix(rep(1, n_samples*n_reps)), fold_changes=FC_scale, lib_sizes=lib_sizes,outdir=paste0(opt$out,'/', opt$prefix), gzip=TRUE, reportCoverage=TRUE, simReads=FALSE)
 
 # hist(log2(reads_per_transcript))
